55c3d5ad1a1bb8e5c2fbb97093be3b90e2aa32ff
Sabry Awad <dr.sabry1997@gmail.com>
2023-05-09 17:26:29 +0300

    🚀 feat(main.rs): Add mouse movement functionality using `robor-rs` crate The code adds functionality to move the mouse cursor and perform click and drag operations using the `robor-rs` crate. The code moves the mouse cursor to a specific position, clicks to activate the window, and then performs a square-shaped movement using drag and drop operations.
    Changes made:
    - The `robor-rs` crate was added as a dependency to the `Cargo.toml` file.
    - The `Mouse` struct from the `robor-rs` crate is imported at the beginning of the file.
    - The `main` function now returns a `Result` type.
    - A new `Mouse` instance is created at the beginning of the `main` function.
    - The mouse cursor is moved to a specific position using the `move_to` method.
    - A click operation is performed using the `click` method.
    - The square-shaped movement is implemented using the `drag_with_duration` method.
    - The `duration` variable is set to a constant value of 200 milliseconds.
    - The `distance` variable is used to control the distance traveled by the mouse cursor.
    - The `change` variable is used to control the decrement in the `distance` variable for each iteration of the loop.
    Signed-off-by: Sabry Awad <dr.sabry1997@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,30 @@
-fn main() {
-    println!("Hello, world!");
+use std::time::Duration;
+
+use robor_rs::Mouse;
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let mut mouse = Mouse::new();
+    // Wait for 5 seconds.
+    std::thread::sleep(Duration::from_secs(5));
+
+    mouse.move_to(300, 400)?;
+    // Click to make the window active.
+    mouse.click()?;
+
+    let mut distance = 300;
+    let change = 20;
+    let duration = Duration::from_millis(200);
+    while distance > 0 {
+        // Move right.
+        mouse.drag_with_duration(distance, 0, duration)?;
+        distance -= change;
+        // Move down.
+        mouse.drag_with_duration(0, distance, duration)?;
+        // Move left.
+        mouse.drag_with_duration(-distance, 0, duration)?;
+        distance -= change;
+        // Move up.
+        mouse.drag_with_duration(0, -distance, duration)?;
+    }
+    Ok(())
 }
