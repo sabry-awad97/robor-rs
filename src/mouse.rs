c51384d75c013e1709fe18634690c4b10207950d
Sabry Awad <dr.sabry1997@gmail.com>
2023-05-09 11:11:40 +0300

    🔬 test(mouse.rs): Add unit tests for MousePosition's to_u32 method ✨ feat(mouse.rs): Implement to_u32 method in MousePosition The `to_u32()` method was added to the `MousePosition` struct to convert its `x` and `y` fields to `u32` values. This is useful when interacting with APIs that require the mouse coordinates to be in `u32` format. The method returns a `Result<(u32, u32), MouseError>` where `MouseError` is an enum type used to handle errors during the conversion.
    Also, a unit test was added to test the new `to_u32()` method. It tests both the successful and failed cases of the conversion.
    
    Changes made:
    - The `MousePosition` struct now has a `to_u32()` method that returns a `Result<(u32, u32), MouseError>` where `MouseError` is an enum type for handling conversion errors.
    - The `MouseError` enum type now has a new `ConversionError` variant for the `to_u32()` method.
    - A unit test was added for the `to_u32()` method.
    Signed-off-by: Sabry Awad <dr.sabry1997@gmail.com>

--- src/mouse.rs
+++ src/mouse.rs
@@ -33,6 +33,18 @@ impl MousePosition {
     pub fn new(x: i32, y: i32) -> Self {
         Self { x, y }
     }
+
+    pub fn to_u32(&self) -> Result<(u32, u32), MouseError> {
+        let x_u32 = self
+            .x
+            .try_into()
+            .map_err(|_| MouseError::ConversionError("Failed to convert x to u32".to_string()))?;
+        let y_u32 = self
+            .y
+            .try_into()
+            .map_err(|_| MouseError::ConversionError("Failed to convert y to u32".to_string()))?;
+        Ok((x_u32, y_u32))
+    }
 }
 
 impl Default for MousePosition {
@@ -73,6 +85,20 @@ mod tests {
         let position = MousePosition::default();
         assert!(position.x >= 0);
         assert!(position.y >= 0);
+    }
+
+    #[test]
+    fn test_mouse_position_to_u32() {
+        let mouse_pos = MousePosition::new(10, 20);
+        let result = mouse_pos.to_u32();
+        assert!(result.is_ok());
+        assert_eq!(result.unwrap(), (10u32, 20u32));
+
+        let mouse_pos = MousePosition::new(-10, 20);
+        assert!(mouse_pos.to_u32().is_err());
+
+        let mouse_pos = MousePosition::new(10, -20);
+        assert!(mouse_pos.to_u32().is_err());
     }
 
     #[test]
