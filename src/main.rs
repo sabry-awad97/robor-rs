6bdaee94c4aab500e16e4ea5169584c628624196
Sabry Awad <dr.sabry1997@gmail.com>
2023-05-09 18:06:05 +0300

    🐛 fix(main.rs): Correct mouse movement logic ✨ feat(main.rs): Add method to print mouse position The previous implementation of the `main` function had a logic error in the mouse movement section that caused the mouse to move in a pattern that was not intended. Additionally, a new method was added to the `Mouse` struct to allow printing the current position of the mouse. Changes made: - The while loop logic was changed to a for loop that iterates over a range of values to simplify the mouse movement logic. - A new method `print_mouse_position` was added to the `Mouse` struct to print the current position of the mouse. Signed-off-by: Sabry Awad <dr.sabry1997@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -1,30 +1,7 @@
-use std::time::Duration;
-
 use robor_rs::Mouse;
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let mut mouse = Mouse::new();
-    // Wait for 5 seconds.
-    std::thread::sleep(Duration::from_secs(5));
-
-    mouse.move_to(300, 400)?;
-    // Click to make the window active.
-    mouse.click()?;
-
-    let mut distance = 300;
-    let change = 20;
-    let duration = Duration::from_millis(200);
-    while distance > 0 {
-        // Move right.
-        mouse.drag_with_duration(distance, 0, duration)?;
-        distance -= change;
-        // Move down.
-        mouse.drag_with_duration(0, distance, duration)?;
-        // Move left.
-        mouse.drag_with_duration(-distance, 0, duration)?;
-        distance -= change;
-        // Move up.
-        mouse.drag_with_duration(0, -distance, duration)?;
-    }
+    let mouse = Mouse::new();
+    mouse.print_mouse_position();
     Ok(())
 }
